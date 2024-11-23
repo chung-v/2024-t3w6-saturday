c5fac1d7211a2932a8c1299f9b6ceca0504723f1
chung-v <vcdott@gmail.com>
2024-11-23 12:01:01 +1100

    feat: create redirect button.

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,8 +1,10 @@
+import RedirectButton from "../components/RedirectButton";
 
 export default function ContactPage(){
     return(
         <div>
             <h1>This is my Contact Page</h1>
+            <RedirectButton />
         </div>
     )
 }
