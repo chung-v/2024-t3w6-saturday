ef5676ae7afa107417da9ba96b23a6a3f7f5751f
chung-v <vcdott@gmail.com>
2024-11-23 11:10:19 +1100

    feat: navbar with links.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar(){
+    return(
+        <nav>
+            <h1>Example Navbar Content</h1>
+            <Link to={"/"}>Home </Link>
+            <Link to={"/about"}>About Me </Link>
+            <Link to={"/contact"}>Contact Me </Link>
+        </nav>
+    )
+}
